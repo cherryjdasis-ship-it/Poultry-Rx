0db44a27a9743db0dc6e1aec582c49de49efbd89
cherryjdasis-ship-it <cherryjd.asis@gmail.com>
2026-09-18 19:46:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.POULTRY_RX_CONFIG = {
   //https://script.google.com/a/macros/bounty.com.ph/s/AKfycbxlR__5UV0AsORxF_O2NSna7R7g9ALoHNfX76A6kbs4KsJ-0UmEN7prm4NNPZlfQHB5LQ/exec
   // Example: https://script.google.com/macros/s/DEPLOYMENT_ID/exec
-  appUrl: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
+  appUrl: "https://script.google.com/a/macros/bounty.com.ph/s/AKfycbxlR__5UV0AsORxF_O2NSna7R7g9ALoHNfX76A6kbs4KsJ-0UmEN7prm4NNPZlfQHB5LQ/exec",
   appName: "Poultry Rx"
 };
